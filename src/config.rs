4b33ce2a09d57f9325b63e1cc77f2a46a4320133
anatawa12 <anatawa12@icloud.com>
2022-10-11 16:14:32 +0900

    feat: implement config loading

---
+++ src/config.rs
@@ -0,0 +1,188 @@
+use crate::{config_file_path, local_low_appdata_path};
+use chrono::format::{Item, StrftimeItems};
+use io::Error;
+use regex::Regex;
+use serde::Serialize;
+use std::io::ErrorKind;
+use std::path::PathBuf;
+use std::{fs, io};
+use toml::Value;
+
+#[derive(Serialize, Debug)]
+pub struct ConfigFile {
+    source: Source,
+    output: Output,
+}
+
+impl ConfigFile {
+    fn read_from_file(&mut self, toml: &Value) -> io::Result<()> {
+        if let Some(source) = toml.get("source") {
+            self.source.read_from_file(source)?
+        }
+        if let Some(output) = toml.get("output") {
+            self.source.read_from_file(output)?
+        }
+        Ok(())
+    }
+}
+
+impl ConfigFile {
+    pub fn source(&self) -> &Source {
+        &self.source
+    }
+    pub fn output(&self) -> &Output {
+        &self.output
+    }
+}
+
+#[derive(Serialize, Debug)]
+pub struct Source {
+    #[serde(skip, default = "Source::folder_default")]
+    folder: PathBuf,
+    #[serde(skip, default = "Source::pattern_default")]
+    pattern: Regex,
+    #[serde(skip, default = "Source::keep_old_default")]
+    keep_old: bool,
+}
+
+impl Source {
+    pub(crate) fn read_from_file(&mut self, toml: &Value) -> io::Result<()> {
+        if let Some(Value::String(str)) = toml.get("folder") {
+            self.folder = PathBuf::from(str)
+        }
+        if let Some(Value::String(str)) = toml.get("pattern") {
+            self.pattern = Regex::new(str).map_err(|e| Error::new(ErrorKind::InvalidData, e))?
+        }
+        if let Some(Value::Boolean(bool)) = toml.get("keep_old") {
+            self.keep_old = *bool;
+        }
+        Ok(())
+    }
+
+    fn folder_default() -> PathBuf {
+        local_low_appdata_path().join("VRChat").join("VRChat")
+    }
+    fn pattern_default() -> Regex {
+        Regex::new(r#"output_log_\d{2}-\d{2}-\d{2}\.txt"#).unwrap()
+    }
+    fn keep_old_default() -> bool {
+        true
+    }
+
+    pub fn folder(&self) -> &PathBuf {
+        &self.folder
+    }
+    pub fn pattern(&self) -> &Regex {
+        &self.pattern
+    }
+    pub fn keep_old(&self) -> bool {
+        self.keep_old
+    }
+}
+
+impl Default for Source {
+    fn default() -> Self {
+        Source {
+            folder: Self::folder_default(),
+            pattern: Self::pattern_default(),
+            keep_old: Self::keep_old_default(),
+        }
+    }
+}
+
+#[derive(Serialize, Debug)]
+pub struct Output {
+    #[serde(skip, default = "Output::folder_default")]
+    folder: PathBuf,
+    #[serde(skip, default = "Output::pattern_default")]
+    pattern: Vec<Item<'static>>,
+    #[serde(skip, default = "Output::utc_time_default")]
+    utc_time: bool,
+}
+
+impl Output {
+    fn folder_default() -> PathBuf {
+        local_low_appdata_path()
+            .join("VRChat")
+            .join("VRChat")
+            .join("logs")
+    }
+    fn pattern_default() -> Vec<Item<'static>> {
+        StrftimeItems::new("output_log_%Y-%m-%d_%H-%M-%S.txt").collect::<Vec<_>>()
+    }
+    fn utc_time_default() -> bool {
+        false
+    }
+
+    pub(crate) fn read_from_file(&mut self, toml: &Value) -> io::Result<()> {
+        if let Some(Value::String(str)) = toml.get("folder") {
+            self.folder = PathBuf::from(str)
+        }
+        if let Some(Value::String(str)) = toml.get("pattern") {
+            fn own_strftime(item: Item) -> Item<'static> {
+                match item {
+                    Item::Literal(s) => Item::OwnedLiteral(s.to_string().into_boxed_str()),
+                    Item::Space(s) => Item::OwnedSpace(s.to_string().into_boxed_str()),
+                    Item::OwnedLiteral(s) => Item::OwnedLiteral(s),
+                    Item::OwnedSpace(s) => Item::OwnedSpace(s),
+                    Item::Numeric(n, p) => Item::Numeric(n, p),
+                    Item::Fixed(f) => Item::Fixed(f),
+                    Item::Error => Item::Error,
+                }
+            }
+            self.pattern = StrftimeItems::new(str).map(own_strftime).collect();
+            if self.pattern.iter().any(|x| matches!(x, Item::Error)) {
+                return Err(Error::new(
+                    ErrorKind::InvalidData,
+                    format!("'{}' is invalid log file pattern", str),
+                ));
+            }
+        }
+        if let Some(Value::Boolean(bool)) = toml.get("utc_time") {
+            self.utc_time = *bool;
+        }
+        Ok(())
+    }
+
+    pub fn folder(&self) -> &PathBuf {
+        &self.folder
+    }
+
+    pub fn pattern(&self) -> &Vec<Item<'static>> {
+        &self.pattern
+    }
+
+    pub fn utc_time(&self) -> bool {
+        self.utc_time
+    }
+}
+
+impl Default for Output {
+    fn default() -> Self {
+        Self {
+            folder: Self::folder_default(),
+            pattern: Self::pattern_default(),
+            utc_time: Self::utc_time_default(),
+        }
+    }
+}
+
+impl Default for ConfigFile {
+    fn default() -> Self {
+        Self {
+            source: Default::default(),
+            output: Default::default(),
+        }
+    }
+}
+
+pub fn read_config() -> io::Result<ConfigFile> {
+    let mut config = ConfigFile::default();
+    match fs::read_to_string(config_file_path()) {
+        Ok(toml) => config.read_from_file(&toml::from_str::<Value>(&toml)?)?,
+        Err(ref e) if e.kind() == ErrorKind::NotFound => {}
+        Err(e) => return Err(e),
+    }
+
+    Ok(config)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+
+use crate::config::read_config;
+use anyhow::{bail, Result};
+use once_cell::race::OnceBox;
+use std::path::{Path, PathBuf};
+use take_if::TakeIf;
+use winsafe::co::{DLGID, KF, KNOWNFOLDERID, MB};
+use winsafe::prelude::user_Hwnd;
+use winsafe::{SHGetKnownFolderPath, HWND};
+
+fn main() -> Result<()> {
+    let config = match read_config() {
+        Ok(config) => config,
+        Err(e) => {
+            eprintln!("error reading config: {:?}", e);
+            let message = format!(
+                "Error reading config file: {}.\nClick OK to discord config & continue.",
+                e
+            );
+            if HWND::GetDesktopWindow().MessageBox(&message, "Error", MB::OKCANCEL)? == DLGID::OK {
+                eprintln!("error ignored, continue with default config");
+                Default::default()
+            } else {
+                bail!(e)
+            }
+        }
+    };
+
+    println!("config loaded: {:#?}", config);
+
+    Ok(())
+}
+
+fn local_low_appdata_path() -> &'static Path {
+    static CELL: OnceBox<PathBuf> = OnceBox::new();
+    CELL.get_or_init(|| {
+        SHGetKnownFolderPath(&KNOWNFOLDERID::LocalAppDataLow, KF::DEFAULT, None)
+            .map(PathBuf::from)
+            .map(Box::new)
+            .expect("getting LocalAppDataLow")
+    })
+}
+
+fn config_file_path() -> &'static Path {
+    static CELL: OnceBox<PathBuf> = OnceBox::new();
+    /// returns read-writable file handle for config
+    fn find_config_file() -> PathBuf {
+        // first, find in exe folder
+        if let Some(config_file) = std::env::current_exe()
+            .ok()
+            .and_then(|p| Some(p.parent()?.join("config.toml")))
+            .take_if(|x| x.exists())
+        {
+            return config_file;
+        }
+
+        // then, create in LocalLow folder
+        local_low_appdata_path().join("vrc-log-renamer/config.toml")
+    }
+
+    CELL.get_or_init(|| Box::new(find_config_file()))
 }
