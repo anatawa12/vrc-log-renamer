262cd5c165c919dfabccda01a98c0172982ec89a
anatawa12 <anatawa12@icloud.com>
2022-10-13 10:33:19 +0900

    feat: implement main rename logic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod config;
 
-use crate::config::{read_config, save_config};
+use crate::config::{read_config, save_config, ConfigFile};
 use anyhow::{bail, Result};
+use chrono::{DateTime, Utc};
 use once_cell::race::OnceBox;
+use std::io::{Read, Write};
 use std::path::{Path, PathBuf};
+use std::{fs, io};
 use take_if::TakeIf;
 use winsafe::co::{DLGID, KF, KNOWNFOLDERID, MB};
 use winsafe::prelude::user_Hwnd;
@@ -27,22 +30,138 @@ fn main() -> Result<()> {
         }
     };
 
-    println!("config loaded: {:#?}", config);
+    println!("config loaded.");
+
+    rename_main(&config)?;
 
     match save_config(&config) {
         Ok(()) => println!("config file written to: {}", config_file_path().display()),
         Err(e) => {
             eprintln!("error writing config: {:?}", e);
-            let message = format!(
-                "Error writing config file: {}.",
-                e
-            );
+            let message = format!("Error writing config file: {}.", e);
             HWND::GetDesktopWindow().MessageBox(&message, "Error", MB::OK)?;
             bail!(e);
         }
     };
 
     Ok(())
+}
+
+fn rename_main(config: &ConfigFile) -> Result<()> {
+    let out_folder = config.output().folder();
+    fs::create_dir_all(out_folder)?;
+    for entry in fs::read_dir(config.source().folder())? {
+        let entry = entry?;
+        if config
+            .source()
+            .pattern()
+            .is_match(&entry.file_name().to_string_lossy())
+        {
+            if let Some(err) = move_log_file(config, &entry.path()).err() {
+                eprintln!("error moving '{}': {}", entry.path().display(), err);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn move_log_file(config: &ConfigFile, path: &Path) -> io::Result<()> {
+    // first, try to open as read to check if the log file is not of running VRChat
+    let mut file = match fs::File::options().write(true).read(true).open(path) {
+        Ok(f) => f,
+        Err(e) => {
+            println!("{} may be used by other process. skipping", path.display());
+            return Ok(());
+        }
+    };
+    // then, assume launch time
+    let date = assume_launch_time(&mut file)?;
+    // now, close the file.
+    drop(file);
+
+    // Data to copy log is ready. Now, move/copy log file.
+
+    fs::create_dir_all(config.output().folder())?;
+    let dst_path = config.output().folder().join(format!(
+        "{}",
+        date.format_with_items(config.output().pattern().iter())
+    ));
+
+    if dst_path.exists() {
+        // if there's file at dst, we assume copy/move is done
+        println!(
+            "{} exists. we assume output log is already copied",
+            dst_path.display()
+        );
+        return Ok(());
+    }
+
+    if config.source().keep_old() {
+        // copy log file
+        fs::copy(path, dst_path)?;
+    } else {
+        // move log file
+        move_file(path, dst_path)?;
+    }
+
+    Ok(())
+}
+
+fn assume_launch_time(f: &mut fs::File) -> io::Result<DateTime<Utc>> {
+    // length of "%Y.%m.%d %H:%M:%S" is 19 bytes
+    let mut buffer = [0 as u8; 19];
+    f.read_exact(&mut buffer)?;
+    // it must be ascii.
+    let str = std::str::from_utf8(&buffer)
+        .map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "invalid utf8"))?;
+    let time_from_log = chrono::NaiveDateTime::parse_from_str(str, "%Y.%m.%d %H:%M:%S")
+        .map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "invalid VRC log"))?;
+
+    /*
+    // TODO: creation time based time zone inference
+    let creation_time = match f.metadata()?.created() {
+        Ok(time) => Some(time),
+        Err(ref e) if e.kind() == io::ErrorKind::Unsupported => None,
+        Err(e) => return Err(e),
+    };
+    let creation_time = creation_time.map(DateTime::<Utc>::from);
+    if let Some(creation_time) = creation_time {
+        // if there's creation time and the minute & second is close to time_from_log,
+        // use time difference between two for time zone inference
+
+    }
+     */
+
+    Ok(time_from_log.and_local_timezone(Utc).earliest().unwrap())
+}
+
+#[cfg(windows)]
+// ERROR_NOT_SAME_DEVICE
+static CROSSES_DEVICES_OS_CODE: i32 = 17;
+
+fn move_file(from: impl AsRef<Path>, to: impl AsRef<Path>) -> io::Result<()> {
+    fn move_by_copy(from: &Path, to: &Path) -> io::Result<()> {
+        let mut from_file = fs::File::options().read(true).write(true).open(from)?;
+        let mut to_file = fs::File::options().create_new(true).write(true).open(to)?;
+        io::copy(&mut from_file, &mut to_file)?;
+        to_file.flush()?;
+        drop(from_file);
+        drop(to_file);
+        fs::remove_file(from)?;
+        Ok(())
+    }
+    fn inner(from: &Path, to: &Path) -> io::Result<()> {
+        match fs::rename(from, to) {
+            Ok(_) => Ok(()),
+            #[cfg(any())] // io_error_more is not stable yet
+            Err(ref e) if e.kind() == io::ErrorKind::CrossesDevices => move_by_copy(from, to),
+            Err(ref e) if e.raw_os_error() == Some(CROSSES_DEVICES_OS_CODE) => {
+                move_by_copy(from, to)
+            }
+            Err(e) => Err(e),
+        }
+    }
+    inner(from.as_ref(), to.as_ref())
 }
 
 fn local_low_appdata_path() -> &'static Path {
