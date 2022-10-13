970e4ea8226249c526d8ad369ab40cf84e07f1db
anatawa12 <anatawa12@icloud.com>
2022-10-13 12:01:15 +0900

    initial gui commit

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ use std::path::{Path, PathBuf};
 use std::{fs, io};
 use take_if::TakeIf;
 use winsafe::co::{DLGID, KF, KNOWNFOLDERID, MB};
-use winsafe::prelude::user_Hwnd;
-use winsafe::{SHGetKnownFolderPath, HWND};
+use winsafe::prelude::{user_Hwnd, GuiNativeControlEvents, GuiWindow};
+use winsafe::{gui, SHGetKnownFolderPath, HWND, POINT, SIZE};
 
 fn main() -> Result<()> {
     let config = match read_config() {
@@ -32,7 +32,8 @@ fn main() -> Result<()> {
 
     println!("config loaded.");
 
-    rename_main(&config)?;
+    MainGUI::new().run()?;
+    //rename_main(&config)?;
 
     match save_config(&config) {
         Ok(()) => println!("config file written to: {}", config_file_path().display()),
@@ -45,6 +46,52 @@ fn main() -> Result<()> {
     };
 
     Ok(())
+}
+
+struct MainGUI {
+    wnd: gui::WindowMain,   // responsible for managing the window
+    btn_hello: gui::Button, // a button
+}
+
+impl MainGUI {
+    pub fn new() -> Self {
+        let wnd = gui::WindowMain::new(
+            // instantiate the window manager
+            gui::WindowMainOpts {
+                title: "My window title".to_owned(),
+                class_icon: gui::Icon::Id(101), // load icon from resource ID 101
+                size: SIZE::new(300, 150),
+                ..Default::default() // leave all other options as default
+            },
+        );
+
+        let btn_hello = gui::Button::new(
+            &wnd, // the window manager is the parent of our button
+            gui::ButtonOpts {
+                text: "&Click me".to_owned(),
+                position: POINT::new(20, 20),
+                ..Default::default()
+            },
+        );
+
+        let new_self = Self { wnd, btn_hello };
+        new_self.events(); // attach our events
+        new_self
+    }
+
+    pub fn run(&self) -> gui::MsgResult<i32> {
+        self.wnd.run_main(None) // simply let the window manager do the hard work
+    }
+
+    fn events(&self) {
+        self.btn_hello.on().bn_clicked({
+            let self2 = self.wnd.clone(); // clone so it can be passed into the closure
+            move || {
+                self2.hwnd().SetWindowText("Hello, world!")?;
+                Ok(())
+            }
+        });
+    }
 }
 
 fn rename_main(config: &ConfigFile) -> Result<()> {
