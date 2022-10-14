eb321b90b94dc34a5be6bb836f17e269d9ba4da7
anatawa12 <anatawa12@icloud.com>
2022-10-14 20:44:43 +0900

    fix way to login

--- src/task_managers.rs
+++ src/task_managers.rs
@@ -1,12 +1,8 @@
-use std::mem::{size_of, zeroed};
-use anyhow::{bail, Result};
-use windows::core::{Interface, InParam, IUnknown, PWSTR, HSTRING, BSTR, GUID};
-use windows::Win32::Foundation::HWND;
-use windows::Win32::Security::Credentials::*;
+use anyhow::{Result};
+use windows::core::{Interface, InParam, PWSTR, HSTRING, BSTR, GUID};
 use windows::Win32::Security::PSECURITY_DESCRIPTOR;
 use windows::Win32::System::TaskScheduler::*;
 use windows::Win32::System::Com::*;
-use windows::Win32::System::Ole::VariantInit;
 
 // see https://learn.microsoft.com/en-us/windows/win32/taskschd/daily-trigger-example--c---
 // see https://learn.microsoft.com/ja-jp/windows/win32/taskschd/c-c-code-example-creating-a-task-using-newworkitem
@@ -56,53 +52,15 @@ pub(crate) fn register_task_manager() -> Result<()> {
         action.SetPath(&std::env::current_exe().unwrap().to_string_lossy().as_ref().into()).unwrap();
         action.SetArguments(&"scheduled".into()).unwrap();
 
-        let info: CREDUI_INFOW = CREDUI_INFOW {
-            cbSize: size_of::<CREDUI_INFOW>() as _,
-            hwndParent: HWND::default(),
-            pszMessageText: (&HSTRING::from("Account info for task register:")).into(),
-            pszCaptionText: (&HSTRING::from("Enter Account Information for Task Registration")).into(),
-            hbmBanner: Default::default(),
-        };
-
-        let mut username = [0 as u16; CREDUI_MAX_USERNAME_LENGTH as usize];
-        let mut password = [0 as u16; /*CREDUI_MAX_PASSWORD_LENGTH as usize*/256];
-
-        let err = CredUIPromptForCredentialsW(
-            Some(&info),
-            &HSTRING::from(""),
-            None,
-            0,
-            &mut username,
-            &mut password,
-            None,
-            CREDUI_FLAGS_GENERIC_CREDENTIALS |  //  Flags
-                CREDUI_FLAGS_ALWAYS_SHOW_UI |
-                CREDUI_FLAGS_DO_NOT_PERSIST
-        );
-        if err != 0 {
-            bail!("Did not get credentials: {err}");
-        }
-
         let _task: IRegisteredTask = root_folder.RegisterTaskDefinition(
             &TASK_NAME.into(),
             &task,
             TASK_CREATE_OR_UPDATE.0,
-            &variant(BSTR::from_raw(&username[0])),
-            &variant(BSTR::from_raw(&password[0])),
-            TASK_LOGON_PASSWORD,
-            &variant(BSTR::from("")),
+            &VARIANT::default(),
+            &VARIANT::default(),
+            TASK_LOGON_INTERACTIVE_TOKEN,
+            &VARIANT::default(),
         ).unwrap();
     }
     Ok(())
-}
-
-// this leaks memory but it's small so I ignore this problem
-fn variant(str: BSTR) -> VARIANT {
-    unsafe {
-        let mut value: VARIANT = zeroed();
-        VariantInit(&mut value as _);
-        (*value.Anonymous.Anonymous).vt = VT_BSTR;
-        *(*value.Anonymous.Anonymous).Anonymous.bstrVal = str;
-        value
-    }
 }
