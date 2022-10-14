beabfe52d9e43fa1f414fec4a1674e6bafe7d560
anatawa12 <anatawa12@icloud.com>
2022-10-14 20:51:04 +0900

    use ? instead of unwrap

--- src/task_managers.rs
+++ src/task_managers.rs
@@ -23,77 +23,52 @@ pub(crate) fn register_task_manager() -> Result<()> {
             None,
             EOLE_AUTHENTICATION_CAPABILITIES(0),
             None,
-        )
-            .unwrap();
+        )?;
 
         let service: ITaskService = CoCreateInstance(
             &GUID::from_u128(0x0f87369f_a4e5_4cfc_bd3e_73e6154572dd), // CLSID_TaskScheduler as _,
             InParam::null(),
             CLSCTX_INPROC_SERVER,
-        )
-            .unwrap();
+        )?;
 
-        service
-            .Connect(
-                InParam::null(),
-                InParam::null(),
-                InParam::null(),
-                InParam::null(),
-            )
-            .unwrap();
+        service.Connect(
+            InParam::null(),
+            InParam::null(),
+            InParam::null(),
+            InParam::null(),
+        )?;
 
-        let root_folder: ITaskFolder = service.GetFolder(&r"\".into()).unwrap();
+        let root_folder: ITaskFolder = service.GetFolder(&r"\".into())?;
 
         // delete if exists
         root_folder.DeleteTask(&TASK_NAME.into(), 0).ok();
 
-        let task: ITaskDefinition = service.NewTask(0).unwrap();
+        let task: ITaskDefinition = service.NewTask(0)?;
         drop(service);
 
         task.RegistrationInfo()?.SetAuthor(&"anatawa12".into())?;
 
         let daily_trigger: IDailyTrigger = task
-            .Triggers()
-            .unwrap()
-            .Create(TASK_TRIGGER_DAILY)
-            .unwrap()
-            .cast::<IDailyTrigger>()
-            .unwrap();
-        daily_trigger.SetId(&"Trigger1".into()).unwrap();
-        daily_trigger
-            .SetStartBoundary(&"2022-10-14T00:00:00".into())
-            .unwrap();
-        daily_trigger.SetDaysInterval(1).unwrap();
+            .Triggers()?
+            .Create(TASK_TRIGGER_DAILY)?
+            .cast::<IDailyTrigger>()?;
+        daily_trigger.SetId(&"Trigger1".into())?;
+        daily_trigger.SetStartBoundary(&"2022-10-14T00:00:00".into())?;
+        daily_trigger.SetDaysInterval(1)?;
 
-        let action: IExecAction = task
-            .Actions()
-            .unwrap()
-            .Create(TASK_ACTION_EXEC)
-            .unwrap()
-            .cast()
-            .unwrap();
-        action
-            .SetPath(
-                &std::env::current_exe()
-                    .unwrap()
-                    .to_string_lossy()
-                    .as_ref()
-                    .into(),
-            )
-            .unwrap();
-        action.SetArguments(&"scheduled".into()).unwrap();
+        let action: IExecAction = task.Actions()?.Create(TASK_ACTION_EXEC)?.cast()?;
+        action.SetPath(&std::env::current_exe()?.to_string_lossy().as_ref().into())?;
+        action.SetArguments(&"scheduled".into())?;
 
-        let _task: IRegisteredTask = root_folder
-            .RegisterTaskDefinition(
-                &TASK_NAME.into(),
-                &task,
-                TASK_CREATE_OR_UPDATE.0,
-                &VARIANT::default(),
-                &VARIANT::default(),
-                TASK_LOGON_INTERACTIVE_TOKEN,
-                &VARIANT::default(),
-            )
-            .unwrap();
+        let _task: IRegisteredTask = root_folder.RegisterTaskDefinition(
+            &TASK_NAME.into(),
+            &task,
+            TASK_CREATE_OR_UPDATE.0,
+            &VARIANT::default(),
+            &VARIANT::default(),
+            TASK_LOGON_INTERACTIVE_TOKEN,
+            &VARIANT::default(),
+        )?;
     }
     Ok(())
 }
@@ -112,26 +87,22 @@ pub(crate) fn unregister_task_manager() -> Result<()> {
             None,
             EOLE_AUTHENTICATION_CAPABILITIES(0),
             None,
-        )
-            .unwrap();
+        )?;
 
         let service: ITaskService = CoCreateInstance(
             &GUID::from_u128(0x0f87369f_a4e5_4cfc_bd3e_73e6154572dd), // CLSID_TaskScheduler as _,
             InParam::null(),
             CLSCTX_INPROC_SERVER,
-        )
-            .unwrap();
+        )?;
 
-        service
-            .Connect(
-                InParam::null(),
-                InParam::null(),
-                InParam::null(),
-                InParam::null(),
-            )
-            .unwrap();
+        service.Connect(
+            InParam::null(),
+            InParam::null(),
+            InParam::null(),
+            InParam::null(),
+        )?;
 
-        let root_folder: ITaskFolder = service.GetFolder(&r"\".into()).unwrap();
+        let root_folder: ITaskFolder = service.GetFolder(&r"\".into())?;
 
         // delete if exists
         root_folder.DeleteTask(&TASK_NAME.into(), 0).ok();
