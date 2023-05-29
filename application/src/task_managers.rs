b534176ed9b7fa0c3f013bed540f9ba1e501c906
anatawa12 <anatawa12@icloud.com>
2023-05-29 15:44:42 +0900

    chore: reimplement task_manager with winsafe

--- application/src/task_managers.rs
+++ application/src/task_managers.rs
@@ -15,87 +15,67 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 use anyhow::Result;
-use windows::core::*;
-use windows::Win32::System::Com::*;
-use windows::Win32::System::TaskScheduler::*;
+use winsafe::prelude::*;
+use winsafe::*;
+use winsafe::co::TASK_ACTION_TYPE;
 
 // see https://learn.microsoft.com/en-us/windows/win32/taskschd/daily-trigger-example--c---
 
 const TASK_NAME: &'static str = "com.anatawa12.vrc-log-renamer";
 
 pub(crate) fn register_task_manager() -> Result<()> {
-    unsafe {
-        CoInitializeEx(None, COINIT_MULTITHREADED)?;
+    let _scope = CoInitializeEx(co::COINIT::MULTITHREADED);
 
-        let service: ITaskService = CoCreateInstance(
-            &GUID::from_u128(0x0f87369f_a4e5_4cfc_bd3e_73e6154572dd), // CLSID_TaskScheduler as _,
-            InParam::null(),
-            CLSCTX_INPROC_SERVER,
-        )?;
+    let service: ITaskService =
+        CoCreateInstance(&co::CLSID::TaskScheduler, None, co::CLSCTX::INPROC_SERVER)?;
 
-        service.Connect(
-            InParam::null(),
-            InParam::null(),
-            InParam::null(),
-            InParam::null(),
-        )?;
+    service.Connect(None, None, None, None)?;
 
-        let root_folder: ITaskFolder = service.GetFolder(&r"\".into())?;
+    let root_folder: ITaskFolder = service.GetFolder(&r"\")?;
 
-        // delete if exists
-        root_folder.DeleteTask(&TASK_NAME.into(), 0).ok();
+    // delete if exists
+    root_folder.DeleteTask(TASK_NAME).ok();
 
-        let task: ITaskDefinition = service.NewTask(0)?;
-        drop(service);
+    let task: ITaskDefinition = service.NewTask()?;
+    drop(service);
 
-        task.RegistrationInfo()?.SetAuthor(&"anatawa12".into())?;
+    task.get_RegistrationInfo()?.put_Author(&"anatawa12")?;
 
-        let daily_trigger: IDailyTrigger = task
-            .Triggers()?
-            .Create(TASK_TRIGGER_DAILY)?
-            .cast::<IDailyTrigger>()?;
-        daily_trigger.SetId(&"Trigger1".into())?;
-        daily_trigger.SetStartBoundary(&"2022-10-14T00:00:00".into())?;
-        daily_trigger.SetDaysInterval(1)?;
+    let daily_trigger: IDailyTrigger = task
+        .get_Triggers()?
+        .Create(co::TASK_TRIGGER_TYPE2::DAILY)?
+        .QueryInterface::<IDailyTrigger>()?;
+    daily_trigger.put_Id(&"Trigger1")?;
+    daily_trigger.put_StartBoundary(&"2022-10-14T00:00:00")?;
+    daily_trigger.put_DaysInterval(1)?;
 
-        let action: IExecAction = task.Actions()?.Create(TASK_ACTION_EXEC)?.cast()?;
-        action.SetPath(&std::env::current_exe()?.to_string_lossy().as_ref().into())?;
-        action.SetArguments(&"scheduled".into())?;
+    let action: IExecAction = task.get_Actions()?.Create(TASK_ACTION_TYPE::EXEC)?.QueryInterface()?;
+    action.put_Path(&std::env::current_exe()?.to_string_lossy().as_ref())?;
+    action.put_Arguments(&"scheduled")?;
 
-        let _task: IRegisteredTask = root_folder.RegisterTaskDefinition(
-            &TASK_NAME.into(),
-            &task,
-            TASK_CREATE_OR_UPDATE.0,
-            &VARIANT::default(),
-            &VARIANT::default(),
-            TASK_LOGON_INTERACTIVE_TOKEN,
-            &VARIANT::default(),
-        )?;
-    }
+    let _task: IRegisteredTask = root_folder.RegisterTaskDefinition(
+        Some(TASK_NAME),
+        &task,
+        co::TASK_CREATION::CREATE_OR_UPDATE,
+        None,
+        None,
+        co::TASK_LOGON::INTERACTIVE_TOKEN,
+        None,
+    )?;
     Ok(())
 }
 
 pub(crate) fn unregister_task_manager() -> Result<()> {
-    unsafe {
-        CoInitializeEx(None, COINIT_MULTITHREADED)?;
+    let _scope = CoInitializeEx(co::COINIT::MULTITHREADED);
 
-        let service: ITaskService = CoCreateInstance(
-            &GUID::from_u128(0x0f87369f_a4e5_4cfc_bd3e_73e6154572dd), // CLSID_TaskScheduler as _,
-            InParam::null(),
-            CLSCTX_INPROC_SERVER,
-        )?;
+    let service: ITaskService =
+        CoCreateInstance(&co::CLSID::TaskScheduler, None, co::CLSCTX::INPROC_SERVER)?;
 
-        service.Connect(
-            InParam::null(),
-            InParam::null(),
-            InParam::null(),
-            InParam::null(),
-        )?;
+    service.Connect(None, None, None, None)?;
 
-        let root_folder: ITaskFolder = service.GetFolder(&r"\".into())?;
+    let root_folder: ITaskFolder = service.GetFolder(r"\")?;
 
-        // delete if exists
-        root_folder.DeleteTask(&TASK_NAME.into(), 0).ok();
-    }
+    // delete if exists
+    root_folder.DeleteTask(TASK_NAME).ok();
     Ok(())
 }
